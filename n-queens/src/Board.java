f2b3d032dc9fd9aa941347d29bb675c7e9607474
kiril <kiril.rusev2000@gmail.com>
2021-10-26 23:11:13 +0300

    Add second homework

---
+++ n-queens/src/Board.java
@@ -0,0 +1,7 @@
+public class Border {
+    private boolean hasConflicts;
+    private int size;
+    private int[] columnConflicts;
+    private int[] diagonal1Conflicts;
+    private int[] diagonal2Conflicts;
+}
